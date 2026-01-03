e3544bad9522fe57dd6f006cd261e2983fea6582
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2026-01-03 23:00:27 +0400

    Add two more event listeners and rewrite my previous textcontent code as a function.

--- Project 6 (Tabs)/index.js
+++ Project 6 (Tabs)/index.js
@@ -1,15 +1,21 @@
-let h2 = document.querySelector('h2');
-let span = document.querySelector('span')
-let tab = ["First Tab", "Second Tab", "Third Tab", "Fourth Tab"];
+document.addEventListener("DOMContentLoaded", () => {
+    let h2 = document.querySelector('h2');
+    let span = document.querySelector('span')
+    let tab = ["First Tab", "Second Tab", "Third Tab", "Fourth Tab"];
 
-h2.textContent = `${tab[0]} content to be displayed here.`;
+    h2.textContent = `${tab[0]} content to be displayed here.`;
 
-let buttons = document.querySelectorAll('button');
+    let buttons = document.querySelectorAll('button');
 
 
-buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
+    function setTab(index) {
         h2.textContent = `${tab[index]} content to be displayed here.`
         span.textContent = `This is ${tab[index]}.`
-    })
+    }
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => { setTab(index) })
+        button.addEventListener('focus', () => { setTab(index) })
+
+
+    });
 });
