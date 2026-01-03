8258b259e9785da8d1b4789e6432a6ad46d87f11
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2026-01-03 22:24:10 +0400

    change javascript

--- Project 6 (Tabs)/index.js
+++ Project 6 (Tabs)/index.js
@@ -1,4 +1,15 @@
 let h2 = document.querySelector('h2');
+let span = document.querySelector('span')
 let tab = ["First Tab", "Second Tab", "Third Tab", "Fourth Tab"];
 
 h2.textContent = `${tab[0]} content to be displayed here.`;
+
+let buttons = document.querySelectorAll('button');
+
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        h2.textContent = `${tab[index]} content to be displayed here.`
+        span.textContent = `This is ${tab[index]}.`
+    })
+});
