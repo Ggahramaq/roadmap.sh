7221c1459f890c60d680f9a6ee6d8003fb65e8e5
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2026-01-04 12:56:23 +0400

    changed buttons to divs so there will be at least one div that is always selected.

--- Project 6 (Tabs)/index.js
+++ Project 6 (Tabs)/index.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let h2 = document.querySelector('h2');
-    let span = document.querySelector('span')
-    let tab = ["First Tab", "Second Tab", "Third Tab", "Fourth Tab"];
+    const h2 = document.querySelector('h2');
+    const span = document.querySelector('span')
+    const tab = ["First Tab", "Second Tab", "Third Tab", "Fourth Tab"];
 
     h2.textContent = `${tab[0]} content to be displayed here.`;
 
-    let buttons = document.querySelectorAll('button');
-
-
+    const buttons = (document.querySelector('.buttons')).querySelectorAll('.button');
+    let previousIndex = 0;
     function setTab(index) {
+        buttons[previousIndex].classList.remove('active');
+        previousIndex = index;
         h2.textContent = `${tab[index]} content to be displayed here.`
         span.textContent = `This is ${tab[index]}.`
+        buttons[index].classList.add('active');
     }
+    setTab(0);
     buttons.forEach((button, index) => {
         button.addEventListener('click', () => { setTab(index) })
-        button.addEventListener('focus', () => { setTab(index) })
     });
 });
